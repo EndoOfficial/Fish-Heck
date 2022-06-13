9df579f5959f391414430f182947f90a4adfd211
zachary-coxsae <1026662@student.sae.edu.au>
2022-06-13 14:33:32 +1000

    V2

--- Shark Bait/Assets/Scripts/GameEvents.cs
+++ Shark Bait/Assets/Scripts/GameEvents.cs
@@ -9,5 +9,7 @@ public static class GameEvents
     public static Action<Vector2, Vector2, float ,int> OnSwipeMove; //movePosition, moveDirection, moveSpeed, touchCount
     public static Action<Vector2, Vector2, float, int> OnSwipeEnd; //endPosition, moveSpeed, touchCount
     public static Action<RaycastHit> OnFishHit; //hitinfo
-    public static Action<Coin> CoinScore; //Player touches coin
+    public static Action CoinScore; //Player touches coin
+    public static Action CoinEat; //When shark eats the coin
+    public static Action platformTiltTrigger; //score has reached threshold to tilt the platform
 }
--- Shark Bait/Assets/Scripts/GameManager.cs
+++ Shark Bait/Assets/Scripts/GameManager.cs
@@ -10,24 +10,63 @@ public class GameManager : MonoBehaviour
     public Image loseScreen;
     public int _playerScore;
     public EventTrigger.TriggerEvent CoinScore;
+    public Transform[] CSpawnPointsLibrary;
+    public Coin coinPrefab; 
+    private Coin currentCoin;
+    private int thresholdCount; // Gives a score threshold for the tilt to occur at 5 point intervals
 
     private void OnEnable()
     {
-        GameEvents.CoinScore += PlayerScores;
+        GameEvents.CoinScore += CoinSpawn;
+        GameEvents.CoinEat += CoinEaten;
     }
     private void OnDisable()
     {
-        GameEvents.CoinScore -= PlayerScores;
+        GameEvents.CoinScore -= CoinSpawn;
+        GameEvents.CoinEat += CoinEaten;
     }
-    public void PlayerScores(Coin playerScores)
-    {
-        _playerScore ++;
-        this.score.text = _playerScore.ToString();
-        Debug.Log("score");
-    }
+
+
     public void RestartGame()
     {
         // restarts game w/ the reset button
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    public void CoinSpawn()
+    {
+        //Pick a random interger from the library then store that transform position
+        int randomInt = Random.Range(0, CSpawnPointsLibrary.Length); 
+        Vector3 randomPosition = CSpawnPointsLibrary[randomInt].position;
+
+        //Use that transform position to clone a prefab coin to 
+        currentCoin = Instantiate(coinPrefab);
+        currentCoin.transform.position = randomPosition;
+
+        //Give the player a point
+        _playerScore++;
+        thresholdCount++;
+        this.score.text = _playerScore.ToString();
+        Debug.Log("score");
+    }
+    public void CoinEaten()
+    {
+        //Pick a random interger from the library then store that transform position
+        int randomInt = Random.Range(0, CSpawnPointsLibrary.Length);
+        Vector3 randomPosition = CSpawnPointsLibrary[randomInt].position;
+
+        //Use that transform position to clone a prefab coin to 
+        currentCoin = Instantiate(coinPrefab);
+        currentCoin.transform.position = randomPosition;
+    }
+    private void Update()
+    {
+     //secret points that will move the platform every 5 points then reset
+        if (thresholdCount == 5)
+        {
+            //GameEvents.platformTiltTrigger.Invoke();
+            thresholdCount = 0;
+            Debug.Log("tilt");
+        }
+        
     }
 }
